bce39dfac4cc0953e11624731c3540493f8b5535
Fl00rMaster <fl00rmasterx@aol.com>
2015-12-07 13:18:13 -0500

    Floaty object
    script for possible collectable objects that float up and down all
    cool-like

---
+++ MonekyNuts/Assets/Scripts/Collectables/LevitationBehavior.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevitationBehavior : MonoBehaviour {
+
+	public float levitationDistance;
+	public float levitationSpeed;
+	public Vector3 startPoint;
+	private bool movingUp;
+	Vector3 top;
+	Vector3 bottom;
+	
+
+	void Start () 
+	{
+		startPoint = transform.position;
+		top = new Vector3 (startPoint.x,startPoint.y + levitationDistance, startPoint.z);
+		bottom = new Vector3 (startPoint.x,startPoint.y - levitationDistance, startPoint.z);
+		movingUp = true;
+	}
+	
+
+	void Update () 
+	{
+		if (transform.position.y >= top.y) 
+		{
+			movingUp = false;
+		}
+		else if (transform.position.y <= bottom.y) 
+		{
+			movingUp = true;
+		}
+		if(movingUp)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, top, levitationSpeed * Time.deltaTime);
+			//transform.position = new Vector3(transform.position.x,transform.position.y + levitationSpeed * Time.deltaTime, transform.position.z);
+		}
+		else
+		{
+			transform.position = Vector3.MoveTowards(transform.position, bottom, levitationSpeed * Time.deltaTime);
+			//GetComponent<Rigidbody> ().MovePosition(bottom);
+			//transform.position = new Vector3(transform.position.x,transform.position.y - levitationSpeed * Time.deltaTime, transform.position.z);
+		}
+	}
+}
