f4efa44f347c1616802499b48bc8fac30ca3e99a
Nalfein16 <dnd_nerd4life@verizon.net>
2015-12-08 14:01:23 -0500

    Player Movement Tweaking and Script Comments
    Commented all scripts I have created.
    
    Also tweaked the player movement so that we can easily add types of
    objects that the player can move to.

--- MonekyNuts/Assets/Scripts/References.cs
+++ MonekyNuts/Assets/Scripts/References.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public static class References {
 
+    // File for commonly referenced objects
     public static StateManager stateManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<StateManager>();
     public static Camera realtimeCamera = GameObject.FindGameObjectWithTag("Camera_Realtime").GetComponent<Camera>();
     public static Camera strategicCamera = GameObject.FindGameObjectWithTag("Camera_Strategic").GetComponent<Camera>();
--- MonekyNuts/Assets/Scripts/Strategy/StrategyCameraMovement.cs
+++ MonekyNuts/Assets/Scripts/Strategy/StrategyCameraMovement.cs
@@ -17,12 +17,15 @@ public class StrategyCameraMovement : MonoBehaviour {
 
     void onStateChange()
     {
+        // If we are in the strategy phase...
         if (References.stateManager.CurrentState == StateManager.states.strategy)
         {
+            // Bring the strategy camera in front of the realtime camera
             camera.depth = 1;
         }
         else
         {
+            // Push the strategy camera behind the realtime camera
             camera.depth = -1;
         }
     }
