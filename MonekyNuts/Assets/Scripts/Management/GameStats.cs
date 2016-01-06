4984eb70031422f93f40a0e64d4ad250cae01cde
Fl00rMaster <fl00rmasterx@aol.com>
2016-01-06 13:13:42 -0500

    Gold Count Thing

---
+++ MonekyNuts/Assets/Scripts/Management/GameStats.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameStats : MonoBehaviour {
+
+    
+    public int score;
+    public int goldCount;
+
+    public Text goldCountText;
+    
+    public void ChangeGoldCount(int amount)
+    {
+        goldCount += amount;
+        goldCountText.text = goldCount.ToString();
+    }
+    public void ChangeScore(int amount)
+    {
+        score += amount;
+    }
+}
