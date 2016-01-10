7bc1e62b951dd06ed5e95a59d1a55f4bfccf91e2
TheChunkierBean <thechunkierbean@gmail.com>
2016-01-10 01:53:19 -0500

    So Animations Work Now (Sorta)
    I need to see how to have it keep playing after mouse click.

---
+++ MonekyNuts/Assets/Models/Animations/Animations.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animations : MonoBehaviour {
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            anim.SetBool("Run", true);
+            Debug.Log("Pressed left click.");
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+	
+	}
+}
