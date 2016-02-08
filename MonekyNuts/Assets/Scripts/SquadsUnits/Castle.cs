b6ab8886510ad31b6dc2ed0a7860d39e884ce6d8
Nalfein16 <dnd_nerd4life@verizon.net>
2016-02-08 14:03:08 -0500

    Quick Changes for Demo
    Castle GUI health.

--- MonekyNuts/Assets/Scripts/SquadsUnits/Castle.cs
+++ MonekyNuts/Assets/Scripts/SquadsUnits/Castle.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Castle : KillableInstance {
 
+    public RectTransform tempHealthBar;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -14,9 +16,29 @@ public class Castle : KillableInstance {
         if (Input.GetKeyDown(KeyCode.K)) Damage(-10);
         
 	}
+    public override void Damage(float amount)
+    {
+        base.Damage(amount);
+        UpdateHealthBar();
+    }
 
     protected override void Die()
     {
         Destroy(gameObject);
+    }
+    public void UpdateHealthBar()
+    {
+        if (currHealth / totHealth > 0)
+        {
+            tempHealthBar.localScale = new Vector3(currHealth / totHealth,
+                                            tempHealthBar.localScale.y,
+                                            tempHealthBar.localScale.z);
+        }
+        else
+        {
+            tempHealthBar.localScale = new Vector3(0,
+                                                tempHealthBar.localScale.y,
+                                                tempHealthBar.localScale.z);
+        }
     }
 }
