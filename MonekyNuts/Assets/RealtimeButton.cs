f5db60a8da3f942526d48e17df58c2c35a138b69
Nalfein16 <dnd_nerd4life@verizon.net>
2016-02-12 13:08:01 -0500

    Added realtime button, fixed timer, added references
    Added a button to go into realtime - you can now stay in strategic for
    as long as you need. Fixed the timer, so it's accurate and only appears
    during realtime. Added references to the References class.

---
+++ MonekyNuts/Assets/RealtimeButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RealtimeButton : MonoBehaviour {
+
+    Image image;
+    Button button;
+    Text text;
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+        button = GetComponent<Button>();
+        text = GetComponentInChildren<Text>();
+        References.stateManager.changeState += onChangeState;
+    }
+
+    void onChangeState()
+    {
+        if (References.stateManager.CurrentState == StateManager.states.realtime)
+        {
+            image.enabled = false;
+            button.enabled = false;
+            text.enabled = false;
+        }
+        else
+        {
+            image.enabled = true;
+            button.enabled = true;
+            text.enabled = true;
+        }
+    }
+
+    public void startRealtime()
+    {
+        References.stateTiming.startRealtimePhase();
+    }
+}
--- MonekyNuts/Assets/Scripts/References.cs
+++ MonekyNuts/Assets/Scripts/References.cs
@@ -4,8 +4,18 @@ using System.Collections;
 // File for commonly referenced objects
 public static class References {
 
-    // Managers and cameras
-    public static StateManager stateManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<StateManager>();
+    // Managers
+    public static GameObject manager = GameObject.FindGameObjectWithTag("Manager");
+    public static StateManager stateManager = manager.GetComponent<StateManager>();
+    public static StateTiming stateTiming = manager.GetComponent<StateTiming>();
+
+    // Game stats and information
+    public static GameStats gameStats = manager.GetComponent<GameStats>();
+    public static Item_Database itemDatabase = manager.GetComponent<Item_Database>();
+    public static Inventory inventory = manager.GetComponent<Inventory>();
+    public static Market market = manager.GetComponent<Market>();
+
+    // Cameras
     public static Camera realtimeCamera = GameObject.FindGameObjectWithTag("Camera_Realtime").GetComponent<Camera>();
     public static Camera strategicCamera = GameObject.FindGameObjectWithTag("Camera_Strategic").GetComponent<Camera>();
 
