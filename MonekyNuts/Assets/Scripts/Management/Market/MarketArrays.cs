8b50cbac7bca0003e03d6004b2ccde5afa8b5970
Fl00rMaster <fl00rmasterx@aol.com>
2016-02-18 13:59:06 -0500

    Push Push Push Push

--- MonekyNuts/Assets/Scripts/Management/Market/MarketArrays.cs
+++ MonekyNuts/Assets/Scripts/Management/Market/MarketArrays.cs
@@ -4,4 +4,9 @@ using System.Collections;
 public class MarketArrays : MonoBehaviour {
 
 	public int[] testItems = new int[] {1,2,3,4,7,8,13,15,17}; //array of item ids
+
+	void Awake()
+	{
+		testItems = new int[] {1, 2, 3, 4, 7, 8 , 13, 15, 17};
+	}
 }
