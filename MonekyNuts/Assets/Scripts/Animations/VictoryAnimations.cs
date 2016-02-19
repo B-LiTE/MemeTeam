53e76001800f16efd5387472cd942d623dca8135
Fl00rMaster <fl00rmasterx@aol.com>
2016-02-18 20:29:35 -0500

    Victory Scene (almost done)

---
+++ MonekyNuts/Assets/Scripts/Animations/VictoryAnimations.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VictoryAnimations : MonoBehaviour {
+
+	[SerializeField]
+	Button button;
+	[SerializeField]
+	Text text;
+
+
+	float fadeRate = .5f;
+
+	public void Awake()
+	{
+		button.gameObject.SetActive(false);
+		text.color = new Color(text.color.r,text.color.g,text.color.b,0);
+		StartCoroutine(Wait ());
+	}
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds(3);
+		StartCoroutine(FadeInVictory());
+	}
+	IEnumerator FadeInVictory()
+	{
+		while(text.color.a < 1)
+		{
+			text.color = new Color(text.color.r,text.color.g,text.color.b,text.color.a + (fadeRate * Time.deltaTime));
+			yield return null;
+		}
+		button.gameObject.SetActive(true);
+		
+		yield return null;
+	}
+	public void BackToMenu()
+	{
+		Application.LoadLevel("Menu");
+	}
+}
