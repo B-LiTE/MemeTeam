b98af2b8f91b1473c6724a086b36a0b608f8021c
Fl00rMaster <fl00rmasterx@aol.com>
2016-02-23 00:39:27 -0500

    stuff among stuff

---
+++ MonekyNuts/Assets/Scripts/UI/Purchase_Castle_Repair.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Purchase_Castle_Repair : MonoBehaviour {
+
+	public int repairPrice;
+
+	void Start()
+	{
+		repairPrice = 10;
+		GetComponentInChildren<Text>().text = "Repair Castle 5% \n<color=#9C0101>Buy Price: " + repairPrice + "</color>";
+	}
+	void PurchaseRepair()
+	{
+		if(References.gameStats.goldCount >= repairPrice)
+		{
+			References.gameStats.ChangeGoldCount(-repairPrice);
+			References.castle.GetComponent<Castle>().ChangeHealth(.05f * References.castle.GetComponent<Castle>().totHealth);
+		}
+	}
+}
